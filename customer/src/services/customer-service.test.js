516b5a16184b3074923108096cf30f22e5e166c4
Rafa <raorcar3@gmail.com>
2024-01-11 22:17:23 +0100

    Initialize repo

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,10 @@
+// which service it is
+describe("CustomerService", () => {
+  // This will explain which function we are testing!
+  describe("SignIn", () => {
+    // Which Scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("Validate response", async () => {});
+  });
+});
